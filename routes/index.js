6b3a87466feff82040c09db6804016b94d68ac86
David Jiang <dnjiang@stanford.edu>
2014-02-26 15:51:40 -0800

    analytics

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,9 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(req, res){
-  	res.render('index', projects);
+  	res.render('index', {'showAlternate': false, 'projects': projects});
+};
+
+exports.viewGrid = function(req, res){
+  	res.render('index', {'showAlternate': true, 'projects': projects});
 };
